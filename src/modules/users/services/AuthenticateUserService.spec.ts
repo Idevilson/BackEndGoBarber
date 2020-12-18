6331f0f495465c1fe45b1e27bd58db87781bbff0
Idevilson Junior <idevilsonjunior@outlook.com>
2020-12-18 14:01:45 -0300

    Arquitetura e testes no NodeJs Finalizados

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,7 +1,8 @@
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
-import AuthenticateUser from './AuthenticateUserService';
+import AuthenticateUserService from './AuthenticateUserService';
 import CreateUserService from './CreateUserService';
+import AppError from '@shared/errors/AppError';
 
 describe('AuthenticateUser', () => {
   it('should be able to authenticate', async () =>{
@@ -13,7 +14,9 @@ describe('AuthenticateUser', () => {
       fakeHashProvider
     );
 
-    const authentitcateUser = new AuthenticateUser(fakeUsersRepository, fakeHashProvider);
+    const authentitcateUser = new AuthenticateUserService(
+       fakeUsersRepository,
+       fakeHashProvider);
 
     const user = await createUser.execute({
       name: 'John Doe',
@@ -30,4 +33,46 @@ describe('AuthenticateUser', () => {
     expect(response.user).toEqual(user);
   });
 
+  it('should not be able to authenticate with a non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to authenticate with wrong password', async () =>{
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    const authentitcateUser = new AuthenticateUserService(
+       fakeUsersRepository,
+       fakeHashProvider);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456'
+    })
+
+    expect(
+      authentitcateUser.execute({
+      email: 'johndoe@example.com',
+      password: 'wrong-password'
+    })).rejects.toBeInstanceOf(AppError);
+  });
 });
--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -5,6 +5,7 @@ import AppError from '@shared/errors/AppError';
 
 import uploadConfig from '@config/upload';
 import IUserRepository from '../repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 import User from '../infra/typeorm/entities/User';
 
@@ -18,6 +19,9 @@ export default class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUserRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -28,17 +32,12 @@ export default class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      // Deletar avatar anterior
-
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
 
     await this.usersRepository.save(user);
 
